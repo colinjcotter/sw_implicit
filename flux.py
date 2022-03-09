cedd3ca406e7ac7d5c4a50c628e94f2ca71f5e2f
Colin Cotter <colin.cotter@imperial.ac.uk>
2022-03-09 17:13:01 +0000

    use mixed formulation for interpolation operator

--- flux.py
+++ flux.py
@@ -1,5 +1,6 @@
 from firedrake import *
 from petsc4py import PETSc
+import numpy as np
 PETSc.Sys.popErrorHandler()
 mesh = PeriodicUnitSquareMesh(20,20)
 
@@ -26,21 +27,17 @@ hup = Upwind('+')*hbar('+') + Upwind('-')*hbar('-')
 u0 = Function(V1, name="u0")
 u0.interpolate(as_vector([-sin(pi*x)*cos(pi*y),
                            cos(pi*x)*sin(pi*y)]))
-W = V1b * U * Tr
+W = V1b * U
 
 def both(e):
     return e('+')+e('-')
 
-w, r, gamma = TestFunctions(W)
-F, v, lambda0 = TrialFunctions(W)
+w, r = TestFunctions(W)
+F, v = TrialFunctions(W)
 a = both(inner(w, n)*inner(F, n))*dS
 a += inner(w, n)*inner(F, n)*ds
 a += inner(w, v)*dx
 a += inner(F, r)*dx
-a += jump(w, n)*lambda0('+')*dS
-a += inner(w, n)*lambda0*ds
-a += jump(F, n)*gamma('+')*dS
-a += inner(F, n)*gamma*ds
 
 L = both(inner(w, n)*inner(u0, n))*hup*dS
 L += inner(w, n)*inner(u0*hbar, n)*ds
@@ -49,17 +46,9 @@ L += inner(u0*hbar, r)*dx
 w0 = Function(W)
 myprob = LinearVariationalProblem(a, L, w0)
 solver_parameters={
-    "mat_type":"matfree",
     "ksp_type":"preonly",
-    'pmat_type':'matfree',
-    'pc_type':'python',
-    'pc_python_type':'firedrake.SCPC',
-    'pc_sc_eliminate_fields': '0, 1',
-    'condensed_field': {'ksp_type': 'gmres',
-                        'ksp_monitor': None,
-                        'ksp_converged_reason': None,
-                        'pc_type': 'ilu'}}
-
+    'pc_type':'lu',
+    'pc_factor_mat_solver_type':'mumps'}
 
 mysolver = LinearVariationalSolver(myprob,
                                    solver_parameters=solver_parameters)
@@ -73,9 +62,36 @@ hform = (
     + (phi('+') - phi('-'))*(un('+')*hbar('+') - un('-')*hbar('-'))*dS
 )
 
-F0, v0 = wo.split()
+F0, v0 = w0.split()
 
-Ftest = phi*div(F0)*dx - action(derivative(hform, ubar), u0)
+F1 = Function(V1)
+
+shapes = (V1.finat_element.space_dimension(),
+          np.prod(V1.shape))
+domain = "{[i,j]: 0 <= i < %d and 0 <= j < %d}" % shapes
+instructions = """
+for i, j
+w[i,j] = w[i,j] + 1
+end
+"""
+weight = Function(V1)
+par_loop((domain, instructions), dx, {"w": (weight, INC)},
+         is_loopy_kernel=True)
+
+instructions = """
+for i, j
+vec_out[i,j] = vec_out[i,j] + vec_in[i,j]/w[i,j]
+end
+"""
+average_kernel = (domain, instructions)
+
+par_loop(average_kernel, dx,
+         {"w": (weight, READ),
+          "vec_in": (F0, READ),
+          "vec_out": (F1, INC)},
+         is_loopy_kernel=True)
+
+Ftest = phi*div(F1)*dx - action(derivative(hform, ubar), u0)
 
 Fass = assemble(Ftest)
 
--- mg.py
+++ mg.py
@@ -21,10 +21,15 @@ class SWTransfer(object):
         # list of flags to say if we've set up before
         self.ready = {}
         # list of coarse and fine fluxes
+        self.w_coarse_b = {} #broken mixed variable for F coarse
+        self.F_coarse_b = {} #pointer to the F part of the above
+        self.coarse_weight = [] #averaging weight
         self.F_coarse = {}
         self.F_fine = {}
         self.u_coarse = {}
-        self.u_fine = {}
+        self.w_fine_b = {} #broken mixed variable for u fine
+        self.u_fine_b = {} #pointer to the u part of the above
+        self.fine_weight = [] #averaging weight
         self.ubar_coarse = {}
         self.ubar_fine = {}
         self.hbar_coarse = {}
@@ -32,7 +37,9 @@ class SWTransfer(object):
         self.coarse_solver = {}
         self.fine_solver = {}
         self.Ftransfer = TransferManager()
-        
+        self.coarse_average_kernel = {}
+        self.fine_average_kernel = {}
+
     def prolong(self, coarse, fine):
         Vfine = FunctionSpace(fine.ufl_domain(),
                               fine.function_space().ufl_element())
@@ -45,24 +52,24 @@ class SWTransfer(object):
             coarse_mesh = coarse.ufl_domain()
             coarse_element = coarse.function_space().ufl_element()
             Vcoarse = FunctionSpace(coarse_mesh, coarse_element)
+            Vcoarseb = FunctionSpace(coarse_mesh,
+                                     BrokenElement(coarse_element))
             degree = self.degree
             
             # make a solver du -> F (on coarse mesh)
-            Tr = FunctionSpace(coarse_mesh, "HDiv Trace", degree)
             bNed = BrokenElement(FiniteElement("N1curl", triangle,
                                                degree-1, variant="integral"))
-            U = FunctionSpace(coarse_mesh, bNed)
-            Wcoarse = Tr * U
+            Ucoarse = FunctionSpace(coarse_mesh, bNed)
+            Wcoarse = Vcoarseb * Ucoarse
 
             Qcoarse = FunctionSpace(coarse_mesh, "DG", degree-1)
-
-            gamma, w = TestFunctions(Wcoarse)
-            F = TrialFunction(Vcoarse)
 
             self.hbar_coarse[key] = Function(Qcoarse)
             self.ubar_coarse[key] = Function(Vcoarse)
             hbar_coarse = self.hbar_coarse[key]
             ubar_coarse = self.ubar_coarse[key]
+            self.w_coarse_b[key] = Function(Wcoarse)
+            self.F_coarse_b[key], _ = self.w_coarse_b[key].split()
             self.F_coarse[key] = Function(Vcoarse)
             self.u_coarse[key] = Function(Vcoarse)
             n = FacetNormal(coarse_mesh)
@@ -70,10 +77,17 @@ class SWTransfer(object):
             hup = Upwind('+')*hbar_coarse('+') + \
                 Upwind('-')*hbar_coarse('-')
 
-            a = gamma('+')*inner(F('+'), n('+'))*dS
-            a += inner(w, F)*dx
-            L = gamma('+')*inner(self.u_coarse[key]('+'), n('+'))*hup*dS
-            L += inner(hbar_coarse*w, self.u_coarse[key])*dx
+            w, r = TestFunctions(Wcoarse)
+            F, v = TrialFunctions(Wcoarse)
+            
+            a = both(inner(w, n)*inner(F, n))*dS
+            a += inner(w, n)*inner(F, n)*ds
+            a += inner(w, v)*dx
+            a += inner(F, r)*dx
+
+            L = both(inner(w, n)*inner(self.u_coarse[key], n))*hup*dS
+            L += inner(w, n)*inner(self.u_coarse[key]*hbar_coarse, n)*ds
+            L += inner(self.u_coarse[key]*hbar_coarse, r)*dx
 
             solver_parameters={
                 "mat_type":"aij",
@@ -81,34 +95,52 @@ class SWTransfer(object):
                 "pc_type":"lu",
                 "pc_factor_mat_solver_type" : "mumps"}
 
-            coarse_prob = LinearVariationalProblem(a, L, self.F_coarse[key],
+            coarse_prob = LinearVariationalProblem(a, L, self.w_coarse_b[key],
                                                    constant_jacobian=False)
             coarse_solver = LinearVariationalSolver(coarse_prob,
                                                     solver_parameters=
                                                     solver_parameters)
             self.coarse_solver[key] = coarse_solver
 
-            #make a solver F -> du (on fine mesh)
+            shapes = (Vcoarse.finat_element.space_dimension(),
+                      np.prod(Vcoarse.shape))
+            domain = "{[i,j]: 0 <= i < %d and 0 <= j < %d}" % shapes
+            instructions = """
+            for i, j
+            w[i,j] = w[i,j] + 1
+            end
+            """
+            self.coarse_weight[key] = Function(Vcoarse)
+            weight = self.coarse_weight[key]
+            par_loop((domain, instructions), dx, {"w": (weight, INC)},
+                     is_loopy_kernel=True)
+            instructions = """
+            for i, j
+            vec_out[i,j] = vec_out[i,j] + vec_in[i,j]/w[i,j]
+            end
+            """
+            self.coarse_average_kernel[key] = (domain, instructions)
+            
+            # make a solver F -> du (on fine mesh)
             fine_mesh = fine.ufl_domain()
             fine_element = fine.function_space().ufl_element()
             Vfine = FunctionSpace(fine_mesh, fine_element)
+            Vfineb = FunctionSpace(fine_mesh,
+                                     BrokenElement(fine_element))
             degree = self.degree
 
-            Tr = FunctionSpace(fine_mesh, "HDiv Trace", degree)
             bNed = BrokenElement(FiniteElement("N1curl", triangle,
                                                degree-1, variant="integral"))
-            U = FunctionSpace(fine_mesh, bNed)
-            Wfine = Tr * U
+            Ufine = FunctionSpace(fine_mesh, bNed)
+            Wfine = Vfineb * Ufine
 
             Qfine = FunctionSpace(fine_mesh, "DG", degree-1)
 
-            gamma, w = TestFunctions(Wfine)
-            F = TrialFunction(Vfine)
-
             self.hbar_fine[key] = Function(Qfine)
             self.ubar_fine[key] = Function(Vfine)
+            self.w_fine_b[key] = Function(Wfine)
+            self.u_fine_b[key], _ = self.w_fine_b[key].split()
             self.F_fine[key] = Function(Vfine)
-            self.u_fine[key] = Function(Vfine)
             hbar_fine = self.hbar_fine[key]
             ubar_fine = self.ubar_fine[key]
             n = FacetNormal(fine_mesh)
@@ -116,31 +148,67 @@ class SWTransfer(object):
             hup = Upwind('+')*hbar_fine('+') + \
                 Upwind('-')*hbar_fine('-')
 
-            gamma, w = TestFunctions(Wfine)
-            u = TrialFunction(Vfine)
-            a = gamma('+')*inner(u('+'), n('+'))*hup*dS
-            a += inner(hbar_fine*w, u)*dx
-            L = gamma('+')*inner(self.F_fine[key]('+'), n('+'))*dS
-            L += inner(w, self.F_fine[key])*dx
+            w, r = TestFunctions(Wfine)
+            u, v = TrialFunctions(Wfine)
 
-            fine_prob = LinearVariationalProblem(a, L, self.u_fine[key],
+            a = both(inner(w, n)*inner(u, n))*hup*dS
+            a += inner(w, n)*inner(u, n)*hbar_fine*ds
+            a += inner(w, v)*hbar_fine*dx
+            a += inner(u, r)*hbar_fine*dx
+
+            F0 = self.F_fine[key]
+            L = both(inner(w, n)*inner(F0, n))*dS
+            L += inner(w, n)*inner(F0, n)*ds
+            L += inner(F0, r)*dx
+            
+            fine_prob = LinearVariationalProblem(a, L, self.w_fine_b[key],
                                                    constant_jacobian=False)
             fine_solver = LinearVariationalSolver(fine_prob,
                                                   solver_parameters=
                                                   solver_parameters)
             self.fine_solver[key] = fine_solver
 
+            shapes = (Vfine.finat_element.space_dimension(),
+                      np.prod(Vfine.shape))
+            domain = "{[i,j]: 0 <= i < %d and 0 <= j < %d}" % shapes
+            instructions = """
+            for i, j
+            w[i,j] = w[i,j] + 1
+            end
+            """
+            self.fine_weight[key] = Function(Vfine)
+            weight = self.fine_weight[key]
+            par_loop((domain, instructions), dx, {"w": (weight, INC)},
+                     is_loopy_kernel=True)
+            instructions = """
+            for i, j
+            vec_out[i,j] = vec_out[i,j] + vec_in[i,j]/w[i,j]
+            end
+            """
+            self.fine_average_kernel[key] = (domain, instructions)
+            
         # update ubar and ubar on the levels
         self.Ftransfer.inject(self.ubar, self.ubar_fine[key])
         self.Ftransfer.inject(self.ubar, self.ubar_coarse[key])
         # copy coarse into the input to the coarse solver
         self.u_coarse[key].assign(coarse)
-        # coarse solver produces F_coarse
-        print("key", key)
+        # coarse solver produces w_coarse_b
+        # This should be replaced with Slate
         self.coarse_solver[key].solve()
+        self.f_coarse[key].assign(0.)
+        par_loop(self.coarse_average_kernel, dx,
+                 {"w": (self.coarse_weight[key], READ),
+                  "vec_in": (self.f_coarse_b[key], READ),
+                  "vec_out": (self.f_coarse[key], INC)},
+                 is_loopy_kernel=True)
         # standard transfer preserves divergence-free subspaces
         self.Ftransfer.prolong(self.F_coarse[key], self.F_fine[key])
-        # fine solver produces u_fine from F_fine
+        # fine solver produces w_fine_b from F_fine
         self.fine_solver[key].solve()
-        # copy u_fine into fine
-        fine.assign(self.u_fine[key])
+        fine.assign(0.)
+        # average u_fine_b (split from w_fine_b) into fine
+        par_loop(self.fine_average_kernel, dx,
+                 {"w": (self.fine_weight[key], READ),
+                  "vec_in": (self.u_fine_b[key], READ),
+                  "vec_out": (fine, INC)},
+                is_loopy_kernel=True)
