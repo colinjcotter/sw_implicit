2bc6e74354f502a8926f5c17ec331498c79d2052
Hiroe Yamazaki <h.yamazaki@imperial.ac.uk>
2024-05-16 11:53:31 +0100

    set arguments, added get_rmsv()

--- compressible_eady_slice.py
+++ compressible_eady_slice.py
@@ -5,10 +5,31 @@ from slice_utils import hydrostatic_rho, pi_formula,\
 from firedrake.output import VTKFile
 import numpy as np
 
+import argparse
+parser = argparse.ArgumentParser(description='Compressible Eady test case.')
+parser.add_argument('--nlayers', type=int, default=30, help='Number of layers. Default 30.')
+parser.add_argument('--ncolumns', type=int, default=30, help='Number of columns. Default 30.')
+parser.add_argument('--c_pen', type=float, default=0., help='Diffusion coeff. Default 0.')
+parser.add_argument('--filename', type=str, default='default', help='filename for pvd')
+parser.add_argument('--tmax', type=float, default=35, help='Final time in hours. Default 24x15=360.')
+parser.add_argument('--dumpt', type=float, default=6, help='Dump time for fields in hours. Default 6.')
+parser.add_argument('--diagt', type=float, default=2, help='Dump time for diagnostics in hours. Default 2.')
+parser.add_argument('--dt', type=float, default=60, help='Timestep in seconds. Default 60.')
+
+args = parser.parse_known_args()
+args = args[0]
+PETSc.Sys.Print(args)
+
+c_pen = fd.Constant(args.c_pen)
+tmax = args.tmax*60*60*24
+dumpt = args.dumpt*60*60
+diagt = args.diagt*60*60
+PETSc.Sys.Print("c_pen = ", c_pen)
+
 dT = fd.Constant(1)
 
-nlayers = 20
-base_columns = 60
+nlayers = args.nlayers
+base_columns = args.ncolumns
 L = 240e3
 distribution_parameters = {"partition": True, "overlap_type": (fd.DistributedMeshOverlapType.VERTEX, 2)}
 
@@ -16,7 +37,7 @@ L = 1000000.
 H = 10000.  # Height position of the model top
 f = 1.e-04
 
-m = fd.PeriodicRectangleMesh(base_columns, ny=1, Lx=2*L, Ly=1.0e-3*L,
+m = fd.PeriodicRectangleMesh(base_columns, ny=1, Lx=2*L, Ly=1.0e-5*L,
                              direction="both",
                              quadrilateral=True,
                              distribution_parameters=distribution_parameters)
@@ -153,6 +174,7 @@ sparameters = {
 Pi0_value = fd.assemble(Pi_ref*fd.dx) / fd.assemble(
     fd.Constant(1.0)*fd.dx(domain=mesh))
 Pi0 = fd.Constant(Pi0_value)
+PETSc.Sys.Print("Pi0 = ", Pi0_value)
 
 # set x-cpt of velocity
 dbdy = -1.0e-07
@@ -198,7 +220,7 @@ f = fd.Constant(f)
 
 eqn = slice_imr_form(un, unp1, rhon, rhonp1, thetan, thetanp1,
                      du, drho, dtheta,
-                     dT=dT, n=n, Up=Up, c_pen=fd.Constant(2.0**(-7./2)),
+                     dT=dT, n=n, Up=Up, c_pen=c_pen,
                      cp=cp, g=g, R_d=R_d, p_0=p_0, kappa=kappa,
                      f = f, Eady={"Pi0":Pi0, "dthetady":dthetady},
                      vector_invariant=False)
@@ -211,15 +233,16 @@ nprob = fd.NonlinearVariationalProblem(eqn, Unp1, bcs=bcs)
 nsolver = fd.NonlinearVariationalSolver(nprob, solver_parameters=sparameters,
                                         options_prefix="nsolver")
 
-name = 'eady_comp_adv'
+name = args.filename
 file_eady = VTKFile(name+'.pvd')
+
 Uns = Un.subfunctions
 un, rhon, thetan = tuple(Uns[i] for i in order)
 
 delta_theta = fd.Function(Vt, name="delta theta").assign(thetan-theta_back)
 delta_rho = fd.Function(V2, name="delta rho").assign(rhon-rho_back)
 
-dt = 60
+dt = args.dt
 dT.assign(dt)
 
 DG0 = fd.FunctionSpace(mesh, "DG", 0)
@@ -242,20 +265,34 @@ file_eady.write(un, rhon, thetan, delta_rho, delta_theta, Courant)
 Unp1.assign(Un)
 
 t = 0.
-day = 60*60*24
 tdump = 0.
-tmax = 10*day
-#tmax = 30*day #  30*day
-dumpt = 0.25*day
-
+tdiag = 0.
 itcount = 0
 stepcount = 0
+
+# calculate rms of v
+def get_rmsv(un, t=None):
+    y_vec = fd.as_vector([0., 1., 0.])
+    y_vel = fd.inner(un, y_vec)
+    DG1 = fd.FunctionSpace(mesh, "DG", 0)
+    c = fd.Function(DG1)
+    c.assign(1)
+    rmsv = fd.sqrt(fd.assemble((fd.dot(y_vel,y_vel))*dx)/fd.assemble(c*dx))
+    PETSc.Sys.Print("rmsv = ", rmsv, "t = ", t)
+    return rmsv
+
+# store the initial rmsv
+rmsv_list = []
+rmsv = get_rmsv(un, t=t)
+rmsv_list.append(rmsv)
+PETSc.Sys.Print("rmsv =", rmsv_list)
 
 PETSc.Sys.Print('tmax', tmax, 'dt', dt)
 while t < tmax - 0.5*dt:
     PETSc.Sys.Print(t)
     t += dt
     tdump += dt
+    tdiag += dt
 
     nsolver.solve()
     Un.assign(Unp1)
@@ -269,6 +306,14 @@ while t < tmax - 0.5*dt:
         file_eady.write(un, rhon, thetan, delta_rho, delta_theta,
                       Courant)
         tdump -= dumpt
+
+    if tdiag > diagt - dt*0.5:
+        # calculate and store rmsv
+        rmsv = get_rmsv(un, t=t)
+        rmsv_list.append(rmsv)
+        PETSc.Sys.Print("rmsv =", rmsv_list)
+        tdiag -= diagt
+
     stepcount += 1
     itcount += nsolver.snes.getLinearSolveIterations()
 PETSc.Sys.Print("Iterations", itcount, "its per step", itcount/stepcount)
